39757dee9871ee719ef44fd2060451659fc06a23
lsh <fantasyz7@naver.com>
2021-05-05 12:15:14 +0900

    mongoClient than mongoose

---
+++ db.js
@@ -0,0 +1,63 @@
+`use strict`;
+const env = require("./.env");
+const { MongoClient } = require("mongodb");
+async function connTest() {
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri = env.mongo.uri;
+  // const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
+
+  //   const client = new MongoClient(uri);
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+    console.log("mongo connected");
+    // Make the appropriate DB calls
+    await listDatabases(client);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+async function mongoClient() {
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri = env.mongo.uri;
+  // const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
+
+  //   const client = new MongoClient(uri);
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+    console.log("mongo connected");
+    // Make the appropriate DB calls
+    return client;
+  } catch (e) {
+    console.error(e);
+    await client.close();
+  }
+}
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
+
+// main().catch(console.error);
+module.exports = { mongoClient, listDatabases };
--- index.js
+++ index.js
@@ -3,25 +3,22 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-// const mongoose = require('mongoose')
+// const env = require("./.env");
 
-const { MongoClient } = require("mongodb");
-const env = require("./.env");
-const uri = env.mongo.uri;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .withSession(() => console.log("mongo arrived"))
-  .catch((err) => console.log(err));
+// const mongoose = require('mongoose')
+// const uri = env.mongo.uri;
+// const mongoClient = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+//   .withSession(() => console.log("mongo arrived"))
+//   .then(() => console.log("mongo connected"))
+//   .catch((err) => console.log(err));
+
+const { mongoClient, listDatabases } = require("./db");
+mongoClient().then((conn) => listDatabases(conn));
 
 app.get("/", (req, res) => {
   res.send("express started");
 });
 app.listen(port, () => console.log(`svr listening on port ${port}`));
-
-// client.connect((err) => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
