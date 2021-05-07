17d49c4bb4339b872cd2bcbf54cb31702673e194
lsh <fantasyz7@naver.com>
2021-05-07 13:44:35 +0900

    db schema

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ nodejs 이전에는 브라우저에서만
 expressjs nodejs 쉽게 시작 프레임워크 중 하나
 
 npm i express mongoose
+
+model > schema
+
+#unstaging
+git rm --cached node_modules -r
---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongo = require("mongoose");
+const userSchema = new mongo.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true, //for spacing
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+const user = mongo.model("User", userSchema);
+module.exports = { user };
