e05abeed69cf32391536400705c6f07270e1ef13
lsh <fantasyz7@naver.com>
2021-05-13 19:48:03 +0900

    mongo deactive

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+
 function LandingPage() {
+  useEffect(() => {
+    axios.get("/api/test").then((response) => console.log(response.data));
+  }, []);
   return <div>LandingPage</div>;
 }
 export default LandingPage;
